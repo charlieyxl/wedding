ef225a93b98c23d4bbac503f5e8d9b710014a9a5
charlieyxl <charlie.yxl@foxmail.com>
2014-10-22 22:38:26 +0800

    Add log function.

--- wedding/src/main/java/com/awoo/wedding/controller/AdminController.java
+++ wedding/src/main/java/com/awoo/wedding/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.awoo.wedding.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,15 @@ import com.awoo.wedding.service.GuestService;
 @RequestMapping("/admin/*")
 public class AdminController
 {
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
 	@Autowired
 	private GuestService guestServe;
 	
 	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public ModelAndView hello(HttpSession httpSession, String username, String password)
 	{
+		logger.info("User {} logged in.", username);
 		httpSession.setAttribute("userid", "1");
 		
 		ModelAndView mav = new ModelAndView();
