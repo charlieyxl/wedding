75b761113803e6a39ee526bcd9cf19c49b61a05b
charlieyxl <charlie.yxl@foxmail.com>
2014-12-22 21:17:04 +0800

    Constants inside interface are all public static final according to JVM specification.

--- wedding/src/main/java/com/awoo/wedding/util/Constants.java
+++ wedding/src/main/java/com/awoo/wedding/util/Constants.java
@@ -2,7 +2,7 @@ package com.awoo.wedding.util;
 
 public interface Constants
 {
-	final String USER_NAME = "username";
-	final String DEFAULT_USER_NAME = "anonymous";
-	final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+	String USER_NAME = "username";
+	String DEFAULT_USER_NAME = "anonymous";
+	String INTERNAL_SERVER_ERROR = "Internal Server Error";
 }
